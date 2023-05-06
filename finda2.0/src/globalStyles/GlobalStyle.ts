ba53ae3f66c360d94cd5ca1abb3e3f6f9fee3c22
SeungHyun <fm10033@gmail.com>
2023-05-06 19:02:18 +0900

    Feat : globalSTyles, theme 구현

--- finda2.0/src/globalStyles/GlobalStyle.ts
+++ finda2.0/src/globalStyles/GlobalStyle.ts
@@ -8,4 +8,34 @@ html{
     margin:0;
     padding:0;
 }
+body {
+		width: 100vw;
+		height: 100vh;
+		font-family: "Noto Sans KR", sans-serif;
+	}
+
+	
+	div {
+		box-sizing: border-box;
+	}
+
+
+	img {
+		width: 100%;
+		height: 100%;
+	}
+
+	button {
+		font: inherit;
+		cursor: pointer;
+	}
+
+	input {
+		font: inherit;
+	}
+
+	a {
+		text-decoration:none;
+    cursor: pointer;
+	}
 `;
