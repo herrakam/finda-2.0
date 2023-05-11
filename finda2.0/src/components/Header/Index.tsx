26723a9609db2ba4685b67b4dc0deec5dfb618d9
SeungHyun <fm10033@gmail.com>
2023-05-11 21:30:56 +0900

    Rename : 헤더 컴포넌트 index로 파일 명 변경

---
+++ finda2.0/src/components/Header/Index.tsx
@@ -0,0 +1,29 @@
+import * as S from '@components/Header/Index.style';
+import MiniSearchBar from '@components/Header/miniSearchBar/Index';
+
+function Header() {
+  const isLogin = true;
+  const PersonalTab = isLogin ? (
+    <>
+      <S.HeaderTab to="/myPick">MY Pick</S.HeaderTab>
+      <S.HeaderTab to="Logout">LogOut</S.HeaderTab>
+    </>
+  ) : (
+    <S.HeaderTab to="/logIn">Login</S.HeaderTab>
+  );
+
+  return (
+    <S.HeaderWrap>
+      <S.HeaderLeft>
+        <S.Title to="/">FINDA</S.Title>
+        <S.HeaderTab to="/movie">Movie</S.HeaderTab>
+        {PersonalTab}
+      </S.HeaderLeft>
+      <S.HeaderRight>
+        <MiniSearchBar />
+      </S.HeaderRight>
+    </S.HeaderWrap>
+  );
+}
+
+export default Header;
