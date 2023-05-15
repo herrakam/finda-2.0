48cb97302da02b3656f49be5e24f3e299a728e5b
SeungHyun <fm10033@gmail.com>
2023-05-15 18:20:15 +0900

    Feat : mainTop 컴포넌트 구현

---
+++ finda2.0/src/components/Main/MainTop/Index.tsx
@@ -0,0 +1,12 @@
+import * as S from '@components/Main/MainTop/Index.style';
+import SearchBar from '@components/Main/SearchBar/Index';
+
+function MainTop() {
+  return (
+    <S.MaintopWrap>
+      <S.Copy>영화가 어디서 서비스중인지 알려드려요!</S.Copy>
+      <SearchBar></SearchBar>
+    </S.MaintopWrap>
+  );
+}
+export default MainTop;
