9b2d0526e6c1317adbc4258ab9bca71cf3fa8380
SeungHyun <fm10033@gmail.com>
2023-05-20 17:39:02 +0900

    Feat : matinbottom 생성

---
+++ finda2.0/src/components/Main/MainBottom/Index.tsx
@@ -0,0 +1,7 @@
+import * as S from '@components/Main/MainBottom/index.style';
+
+function MainBottom() {
+  return <S.MainBottomWrap>메인 아래</S.MainBottomWrap>;
+}
+
+export default MainBottom;
--- finda2.0/src/pages/Main.tsx
+++ finda2.0/src/pages/Main.tsx
@@ -1,9 +1,11 @@
+import MainBottom from '@components/Main/MainBottom/Index';
 import MainTop from '@components/Main/MainTop/Index';
 
 function Main() {
   return (
     <>
       <MainTop />
+      <MainBottom />
     </>
   );
 }
