e0b7b436fb9f0ea556a4ae18f0fca679d4e5dc1c
SeungHyun <fm10033@gmail.com>
2023-05-22 18:59:49 +0900

    Refact : eslint 옵션 변경
    line-break 제거
    comma-dangle 객체만 마지막에 콤마 필요한 규칙으로 변경

--- finda2.0/.eslintrc.cjs
+++ finda2.0/.eslintrc.cjs
@@ -1,53 +1,85 @@
 module.exports = {
   env: {
     browser: true,
+
     es2021: true,
   },
+
   extends: ['airbnb', 'plugin:react/recommended', 'standard-with-typescript'],
+
   overrides: [],
+
   parserOptions: {
     project: './tsconfig.json',
+
     ecmaVersion: 'latest',
+
     sourceType: 'module',
   },
+
   plugins: ['react'],
+
   rules: {
-    'linebreak-style': ['error', 'windows'],
     'import/no-unresolved': 'error',
+
     'react/jsx-filename-extension': [
       2,
+
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
+
     'prettier/prettier': [
       'error',
+
       {
         endOfLine: 'auto',
       },
     ],
-    'comma-dangle': [2],
+
+    'comma-dangle': [
+      'error',
+      {
+        array: 'ignore',
+        object: 'always',
+      },
+    ],
+
     'import/order': [
       'error',
+
       {
         groups: [
           'builtin',
+
           'external',
+
           'internal',
+
           'sibling',
+
           'parent',
+
           'index',
         ],
+
         pathGroups: [
           {
             pattern: '@',
+
             group: 'internal',
+
             position: 'before',
           },
         ],
+
         pathGroupsExcludedImportTypes: ['internal'],
+
         alphabetize: {
           order: 'asc',
+
           caseInsensitive: true,
         },
+
         'newlines-between': 'always',
       },
     ],
