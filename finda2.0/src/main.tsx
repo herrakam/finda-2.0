0d0810a96ed17491520031f9cad55042a9c3707d
SeungHyun <fm10033@gmail.com>
2023-06-04 17:06:53 +0900

    Feat : 로그인 정보 조건부로 전달하는 base 구현

--- finda2.0/src/main.tsx
+++ finda2.0/src/main.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "@/App";
-import { GlobalStyle } from "@/globalStyles/GlobalStyle";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+import { GlobalStyle } from '@/globalStyles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { darkMode } from '@/globalStyles/GlobalTheme';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={darkMode}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
