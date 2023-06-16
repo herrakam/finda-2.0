ded70c82519eb8dcf8fc3de14bf60a621497b4fd
SeungHyun <fm10033@gmail.com>
2023-06-16 21:25:39 +0900

    Feat : mainTop 내부 컨텐츠 배치 준비 완료

--- finda2.0/src/components/Main/MainBottom/Index.tsx
+++ finda2.0/src/components/Main/MainBottom/Index.tsx
@@ -1,7 +1,7 @@
 import * as S from '@components/Main/MainBottom/index.style';
 
 function MainBottom() {
-  return <S.MainBottomWrap>메인 아래</S.MainBottomWrap>;
+  return <S.MainBottomWrap></S.MainBottomWrap>;
 }
 
 export default MainBottom;
--- finda2.0/src/components/Main/MainBottom/index.style.ts
+++ finda2.0/src/components/Main/MainBottom/index.style.ts
@@ -1,6 +1,9 @@
+import { mixin } from '@/globalStyles/GlobalStyle';
 import styled from 'styled-components';
 
 export const MainBottomWrap = styled.div`
   width: 100%;
   height: 100vh;
+  ${mixin.flexbox({ dir: 'column', vertical: 'center', horizontal: 'center' })}
+  gap : 20px;
 `;
