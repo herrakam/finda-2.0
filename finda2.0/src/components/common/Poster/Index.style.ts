a07996d99bc1f78880ad867db97f6ca934465a0f
SeungHyun <fm10033@gmail.com>
2023-06-17 17:56:57 +0900

    Feat : poster component 구현 완료

--- finda2.0/src/components/common/Poster/Index.style.ts
+++ finda2.0/src/components/common/Poster/Index.style.ts
@@ -1,3 +1,4 @@
+import { mixin } from '@/globalStyles/GlobalStyle';
 import styled from 'styled-components';
 
 export const PosterWrap = styled.figure`
@@ -5,4 +6,17 @@ export const PosterWrap = styled.figure`
   height: 300px;
   border-radius: 5px;
   transition: 0.5s;
+`;
+export const Posterimage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+`;
+export const PosterTitle = styled.div<{ isHover: boolean }>`
+  width: 100%;
+  height: 100px;
+  ${mixin.flexbox({ vertical: 'center' })}
+  padding:10px;
+  ${({ theme }) => theme.typography.Light}
+  color:${({ theme }) => theme.pallete.normalFont}
 `;
--- finda2.0/src/components/common/Poster/Index.tsx
+++ finda2.0/src/components/common/Poster/Index.tsx
@@ -1,5 +1,26 @@
-function Poster() {
-  return <>포스터</>;
+import Dimmed from '@components/common/Dimmed/Index';
+import * as S from '@components/common/Poster/Index.style';
+import { useState } from 'react';
+
+type posterType = {
+  title: string;
+  src: string;
+};
+
+function Poster({ title, src }: posterType) {
+  const [isHover, setHover] = useState(false);
+
+  function showDimmed() {
+    setHover(true);
+  }
+  return (
+    <Dimmed size="poster" isHover={isHover}>
+      <S.PosterWrap onMouseEnter={showDimmed}>
+        <S.Posterimage src={src} />
+        <S.PosterTitle isHover={isHover}>{title}</S.PosterTitle>
+      </S.PosterWrap>
+    </Dimmed>
+  );
 }
 
 export default Poster;
