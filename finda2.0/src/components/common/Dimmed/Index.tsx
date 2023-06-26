f9722b600cd22d19c60e4baf00e0ef38ee31d0ae
SeungHyun <fm10033@gmail.com>
2023-06-26 18:05:04 +0900

    Refact : dimmed 크기 설정 에러 변경

--- finda2.0/src/components/common/Dimmed/Index.tsx
+++ finda2.0/src/components/common/Dimmed/Index.tsx
@@ -3,8 +3,8 @@ import { DimSizeType, DimmedType } from '@components/common/Dimmed/type';
 
 function Dimmed({ size, isHover, children }: DimmedType) {
   const dimSize: DimSizeType = size
-    ? { width: `100vw`, height: `100vh` }
-    : { width: `200px`, height: `300px` };
+    ? { width: `200px`, height: `300px` }
+    : { width: `100vw`, height: `100vh` };
   return (
     <S.DimmedWrap size={dimSize} isHover={isHover}>
       {children}
