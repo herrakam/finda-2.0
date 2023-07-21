89744b3bb5846c8cd363d4f3ae12b22f1806454f
SeungHyun <fm10033@gmail.com>
2023-07-21 17:50:57 +0900

    Feat : mainBottom 페이지에 Rank 컴포넌트 랜더링

--- finda2.0/src/components/Main/MainBottom/Index.tsx
+++ finda2.0/src/components/Main/MainBottom/Index.tsx
@@ -1,13 +1,42 @@
 import * as S from '@components/Main/MainBottom/index.style';
-import Poster from '@components/common/Poster/Index';
+import Rank from '@components/common/Rank';
+import { RankType } from '@components/common/Rank/type';
 
 function MainBottom() {
+  const RankProps: RankType = {
+    subject: 'top 5',
+    rankInfo: [
+      {
+        title: 'sample',
+        imgSrc:
+          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+      },
+      {
+        title: 'sample',
+        imgSrc:
+          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+      },
+      {
+        title: 'sample',
+        imgSrc:
+          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+      },
+      {
+        title: 'sample',
+        imgSrc:
+          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+      },
+      {
+        title: 'sample',
+        imgSrc:
+          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+      },
+    ],
+  };
   return (
     <S.MainBottomWrap>
-      <Poster
-        title="sample"
-        src="https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp"
-      />
+      <Rank {...RankProps} />
+      <Rank {...RankProps} />
     </S.MainBottomWrap>
   );
 }
--- finda2.0/src/components/Main/MainBottom/index.style.ts
+++ finda2.0/src/components/Main/MainBottom/index.style.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 export const MainBottomWrap = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   ${mixin.flexbox({ dir: 'column', vertical: 'center', horizontal: 'center' })}
   gap : 20px;
+  background: ${({ theme }) => theme.pallete.normalBg};
+  position: relative;
 `;
