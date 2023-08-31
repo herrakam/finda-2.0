64f81c54107fe618fe72a5bc04045d8278dacd64
SeungHyun <fm10033@gmail.com>
2023-08-31 17:50:53 +0900

    Feat : tanstack query 사용 환경 조성
    queryClient 구현

--- finda2.0/src/main.tsx
+++ finda2.0/src/main.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import { GlobalStyle } from '@/globalStyles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      useErrorBoundary: true,
+    },
+    mutations: {
+      useErrorBoundary: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
