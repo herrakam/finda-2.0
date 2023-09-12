7cbd583e7bf67775580755fca06ff51111ca9530
herrakam <fm10033@gmail.com>
2023-09-13 02:14:28 +0900

    Refact: 환경변수 적용
    환경 변수 파일 생성
    
    환경 변수 참조하는 방식으로 변경

--- finda2.0/vite.config.ts
+++ finda2.0/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@pages", replacement: "/src/pages" },
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
     ],
+  },
+  define: {
+    'process.env': process.env,
   },
 });
