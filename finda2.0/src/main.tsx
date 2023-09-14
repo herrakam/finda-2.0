f14cc1588e57613c03f68233cff18db0ce4b51b8
herrakam <fm10033@gmail.com>
2023-09-14 16:58:29 +0900

    Feat : tanstack-query devtools 사용 환경 설정

--- finda2.0/src/main.tsx
+++ finda2.0/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import { GlobalStyle } from '@/globalStyles/GlobalStyle';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,10 +18,12 @@ export const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <GlobalStyle />
-    <App />
-  </QueryClientProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
+    ,
+  </React.StrictMode>,
 );
