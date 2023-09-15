9fd7f11f9c1f031a4208d87f40f71558889ebe5c
herrakam <fm10033@gmail.com>
2023-09-15 14:05:07 +0900

    Feat : 데이터 스크래핑 구현
    result버튼 클릭할 때마다 resultData 추가로 가져옴
    detail 버튼 클릭시 detailData 추가로 가져옴

--- finda2.0/src/pages/Scrap.tsx
+++ finda2.0/src/pages/Scrap.tsx
@@ -1,3 +1,4 @@
+import { PLATFORMINFO } from '@/assets/static';
 import { mixin } from '@/globalStyles/GlobalStyle';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
@@ -11,59 +12,233 @@ interface ItemType {
   poster: string;
   object_type: string;
 }
+interface NormalizedItemType {
+  id: number;
+  title: string;
+  poster: string;
+  object_type: string;
+}
+interface CreditType {
+  role: string;
+  person_id: number;
+  name: string;
+}
+interface OfferType {
+  jw_entity_id: string;
+  monetization_type: 'buy' | 'rent' | 'flatrate';
+  provider_id: number;
+  package_short_name: string;
+  retail_price?: number;
+  last_change_retail_price?: number;
+  last_change_difference?: number;
+  last_change_percent?: number;
+  last_change_date?: number;
+  last_change_date_provider_id?: string;
+  currency: string;
+  urls: {
+    standard_web: string;
+    [key: string]: string;
+  };
+  available_from: string;
+  available_to: string;
+  presentation_type: string;
+  country: string;
+}
+interface NormalizedOfferType {
+  url: string;
+  type: 'buy' | 'rent' | 'flatrate';
+  price?: number;
+  provider: string;
+  iconSrc: string;
+}
+interface NormalizedDetailType {
+  backdropImgUrl: string;
+  title: string;
+  originTitle: string;
+  releasedYear: string;
+  genreArr: number[];
+  runtime: number;
+  director: string;
+  actors: CreditType[];
+  offerArr: NormalizedOfferType[];
+  disc: string;
+}
 
-// const getDetailData = async (id: number) => {
-//   const DETAILURL = `https://apis.justwatch.com/content/titles/movie/${id}/locale/ko_KR?language=ko`;
-//   const data = (await axios.get(DETAILURL)).data;
-//   // const [genreArr, offerArr, title, discription, runtime, creditsArr];
-// };
+const getPriceAndType = (offer: OfferType) => {
+  let priceAndType: { type: 'buy' | 'rent' | 'flatrate'; price?: number };
+  switch (offer.monetization_type) {
+    case 'buy':
+      priceAndType = {
+        type: offer.monetization_type,
+        price: offer.retail_price,
+      };
+      break;
+    case 'flatrate':
+      priceAndType = {
+        type: offer.monetization_type,
+      };
+      break;
+    case 'rent':
+      priceAndType = {
+        type: offer.monetization_type,
+        price: offer.retail_price,
+      };
+      break;
+  }
+  return priceAndType;
+};
+
+const getOfferPlatformInfo = (offer: OfferType) => {
+  const info = PLATFORMINFO.find(
+    platformInfo => offer.provider_id === platformInfo.id,
+  );
+  return { provider: info?.name!, iconSrc: info?.icon_url! };
+};
+
+const getNormalizedOffer = (offers: OfferType[]) => {
+  const normalizedOffer: NormalizedOfferType[] = offers.map(
+    (offer: OfferType) => {
+      return {
+        url: offer.urls.standard_web,
+        ...getOfferPlatformInfo(offer),
+        ...getPriceAndType(offer),
+      };
+    },
+  );
+
+  return normalizedOffer;
+};
 
 function Scrap() {
   const [pageNum, setPageNum] = useState(0);
   const [isClicked, setIsclicked] = useState(false);
-  const [posterData, setPosterData] = useState<ItemType[]>([]);
+  const [isDetailClicked, setIsDetailClicked] = useState(false);
+  const [idArr, setIdArr] = useState<number[]>([]);
+  const [posterData, setPosterData] = useState<NormalizedItemType[]>([]);
+  const [detailData, setDetailData] = useState<NormalizedDetailType[]>([]);
   const POSTERTITLEURL = `https://apis.justwatch.com/content/titles/ko_KR/popular?body=%7B%22fields%22:[%22id%22,%22title%22,%22poster%22,%22object_type%22],%22content_types%22:[%22movie%22],%22monetization_types%22:[%22ads%22,%22buy%22,%22flatrate%22,%22rent%22,%22free%22],%22page%22:${pageNum},%22page_size%22:40,%22matching_offers_only%22:false%7D`;
 
   async function getContentData() {
     const posterData = (await axios.get(POSTERTITLEURL)).data.items;
-    return posterData;
+    const normalizedPosterData: NormalizedItemType[] = posterData.map(
+      (info: ItemType) => {
+        return {
+          id: info.id,
+          title: info.title,
+          poster: `https://images.justwatch.com/poster/${info.id}/s332/`,
+          objectType: info.object_type,
+        };
+      },
+    );
+    return normalizedPosterData;
   }
 
-  const updateData = (data: ItemType[]) => {
-    setPosterData(posterData.concat(data));
+  async function getDetailData(id: number) {
+    const DETAILURL = `https://apis.justwatch.com/content/titles/movie/${id}/locale/ko_KR?language=ko`;
+    const data = (await axios.get(DETAILURL)).data;
+    const normalizedData: NormalizedDetailType = {
+      backdropImgUrl: `https://images.justwatch.com/backdrop/${id}/s1440`,
+      title: data.title,
+      originTitle: data.original_title,
+      releasedYear: data.original_release_year,
+      genreArr: data.genre_ids,
+      runtime: data.runtime,
+      director: data.credits.filter(
+        (credit: CreditType) => credit.role === 'DIRECTOR',
+      ).name,
+      actors: data.credits.filter(
+        (credit: CreditType, idx: number) =>
+          credit.role === 'ACTOR' && idx > 10,
+      ).name,
+      offerArr: getNormalizedOffer(data.offers),
+      disc: data.short_discription,
+    };
+    return normalizedData;
+  }
+  const updatePosterData = (data: NormalizedItemType[]) => {
+    setPosterData([...posterData, ...data]);
+  };
+
+  const updateDetailData = (data: NormalizedDetailType[]) => {
+    setDetailData([...detailData, ...data]);
   };
 
   const increasePage = () => {
     setPageNum(pageNum + 1);
   };
+  const getIdInfoArr = (data: ItemType[]) => {
+    const idArrInfo = data.map((item: ItemType) => item.id);
+    setIdArr(idArrInfo);
+  };
 
-  // const [detailData, setDetailData] = useState([]);
   const getContentInfo = () => {
-    const { data } = useQuery(['posterQueryKey', pageNum], getContentData, {
-      enabled: isClicked,
-      onSuccess: data => {
-        setIsclicked(false);
-        updateData(data);
-        console.log(posterData);
+    const { data, status } = useQuery(
+      ['posterQueryKey', pageNum],
+      getContentData,
+      {
+        enabled: isClicked,
+        retry: false,
       },
-    });
+    );
+    if (status === 'success') {
+      setIsclicked(false);
+      updatePosterData(data);
+    }
     return data as ItemType[];
   };
 
-  getContentInfo();
+  const getDetailInfo = () => {
+    const { data, isSuccess } = useQuery(
+      ['detailQuerykey'],
+      () => {
+        const resultArr = idArr.map((id: number) => getDetailData(id));
+        return Promise.all(resultArr);
+      },
+      {
+        enabled: isDetailClicked,
+        retry: false,
+      },
+    );
+    if (isSuccess) {
+      setIsDetailClicked(false);
+      updateDetailData(data);
+      // return data;
+    }
+    return data;
+  };
+
+  const data = getContentInfo();
+  getDetailInfo();
+
+  const showDetail = posterData.map(
+    (detail: NormalizedItemType, idx: number) => (
+      <div>{`${idx} ${detail.title}`}</div>
+    ),
+  );
+
   return (
-    <Wrap>
-      <GetDataBtn
-        onClick={() => {
-          setIsclicked(true);
-          increasePage();
-        }}
-      >
-        포스터,제목데이터 가져오기
-      </GetDataBtn>
-      <GetDetailBtn>디테일 데이터 가져오기</GetDetailBtn>
-      <PostDataBtn>데이터 보내기</PostDataBtn>
-    </Wrap>
+    <>
+      <Wrap>
+        <GetDataBtn
+          onClick={() => {
+            setIsclicked(true);
+            increasePage();
+          }}
+        >
+          포스터,제목데이터 가져오기
+        </GetDataBtn>
+        <GetDetailBtn
+          onClick={() => {
+            setIsDetailClicked(true);
+            getIdInfoArr(data);
+          }}
+        >
+          디테일 데이터 가져오기
+        </GetDetailBtn>
+        <PostDataBtn>데이터 보내기</PostDataBtn>
+      </Wrap>
+      <ShowResult>{showDetail}</ShowResult>
+    </>
   );
 }
 
@@ -74,7 +249,7 @@ const Wrap = styled.div`
   ${mixin.flexbox({ vertical: 'center', horizontal: 'center' })}
   gap: 40px;
 `;
-
+const ShowResult = styled.div``;
 const GetDataBtn = styled.button`
   width: 100px;
   height: 50px;
