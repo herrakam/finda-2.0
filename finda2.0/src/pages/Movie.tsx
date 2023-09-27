17b7d4cec4bc0a1e32abe3e9976ece4628d59e8c
herrakam <fm10033@gmail.com>
2023-09-28 01:13:05 +0900

    Feat : url에서 파라미터 추출해서 데이터 가져오는 기능 구현

--- finda2.0/src/pages/Movie.tsx
+++ finda2.0/src/pages/Movie.tsx
@@ -1,9 +1,26 @@
+import { db } from '@/Firebase';
 import ContentInfo from '@components/ContentInfo/Index';
 import PageContainer from '@components/common/PageContainer/Index';
+import { useQuery } from '@tanstack/react-query';
+import { doc, getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
 
 function Movie() {
-  const { params } = useParams();
+  const { contentTitle } = useParams() as { contentTitle: string };
+
+  const getMovieData = async () => {
+    const snap = await getDoc(doc(db, 'movies', contentTitle));
+    return snap?.data();
+  };
+
+  const getMovieInfo = () => {
+    return useQuery(['getMovieInfoQueryKey'], getMovieData);
+  };
+  const { data } = getMovieInfo();
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <PageContainer>
       <ContentInfo />
