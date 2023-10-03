c67737d2cb0f52292b10a60c33c1dd75dfc425f9
herrakam <fm10033@gmail.com>
2023-10-03 17:55:25 +0900

    Feat : ContentInfo offerArr 제외 구현 완료

--- finda2.0/src/components/ContentInfo/Index.style.ts
+++ finda2.0/src/components/ContentInfo/Index.style.ts
@@ -1,6 +1,99 @@
+import { mixin } from '@/globalStyles/GlobalStyle';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 80%;
   min-height: 100vh;
   background: ${({ theme }) => theme.pallete.normalBg};
+`;
+export const BackDropImg = styled.img`
+  position: absolute;
+  left: 0;
+  object-fit: cover;
+  object-position: center 25%;
+  width: 100%;
+  height: 45vh;
+  min-height: 200px;
+`;
+
+export const InfoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 20px;
+  min-height: 80vh;
+  margin-top: 35vh;
+  background: ${({ theme }) => theme.pallete.normalBg};
+  border-radius: 25px 25px 0 0;
+  z-index: 1;
+  ${mixin.flexbox({})};
+  gap: 20px;
+  margin-bottom: 50px;
+`;
+export const InfoLeft = styled.div`
+  ${mixin.flexbox({ dir: 'column' })};
+  gap: 15px;
+`;
+export const InfoRight = styled(InfoLeft)`
+  gap: 10px;
+`;
+
+export const Poster = styled.img`
+  border-radius: 5px;
+  height: 500px;
+`;
+export const GenreContainer = styled.div`
+  ${mixin.flexbox({})}
+  gap: 10px;
+`;
+export const FlexContainer = styled.div`
+  ${mixin.flexbox({ vertical: 'flex-end' })};
+  gap: 10px;
+  margin-bottom: 5px;
+`;
+
+export const ActorsContainer = styled.div`
+  display: grid;
+  grid-auto-rows: 15px;
+  grid-template-columns: repeat(5, minmax(max-content, max-content));
+  place-content: start center;
+  gap: 15px;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  margin-bottom: 5px;
+`;
+export const Title = styled.span`
+  ${({ theme }) => theme.typography.Title};
+  color: ${({ theme }) => theme.pallete.normalFont};
+`;
+
+export const ReleasedYear = styled(Title)`
+  ${({ theme }) => theme.typography.Bold};
+`;
+export const RealTitle = styled.div`
+  color: ${({ theme }) => theme.pallete.grey4};
+  margin-bottom: 5px;
+`;
+
+export const ItemTitle = styled.div`
+  ${({ theme }) => theme.typography.Bold};
+  color: ${({ theme }) => theme.pallete.normalFont};
+`;
+
+export const ItemContent = styled.div`
+  ${({ theme }) => theme.typography.Regular};
+  color: ${({ theme }) => theme.pallete.normalFont};
+  margin-bottom: 5px;
+`;
+
+export const ItemConentInline = styled.span`
+  ${({ theme }) => theme.typography.Regular};
+  color: ${({ theme }) => theme.pallete.normalFont};
+`;
+
+export const DiscContatiner = styled.div`
+  ${mixin.flexbox({ dir: 'column' })}
+  gap: 20px;
+`;
+export const DiscContent = styled(ItemContent)`
+  line-height: 2rem;
 `;
--- finda2.0/src/components/ContentInfo/Index.tsx
+++ finda2.0/src/components/ContentInfo/Index.tsx
@@ -1,7 +1,62 @@
+import { GENREINFO } from '@/assets/static';
 import * as S from '@/components/ContentInfo/Index.style';
+import { GenreType, NormalizedDetailType } from '@/utils/type';
 
-function ContentInfo() {
-  return <S.Container>영화 상세 페이지</S.Container>;
+function ContentInfo({
+  backdropImgUrl,
+  actors,
+  director,
+  disc,
+  genreArr,
+  offerArr,
+  originTitle,
+  releasedYear,
+  runtime,
+  title,
+  poster,
+}: NormalizedDetailType) {
+  const actorsContent =
+    actors === '출연진 정보 없음'
+      ? actors
+      : actors.map((actor: string) => (
+          <S.ItemConentInline key={actor}>{actor}</S.ItemConentInline>
+        ));
+
+  const genreContent = genreArr.map((genre: number) => (
+    <S.ItemConentInline key={genre}>
+      {GENREINFO.filter((info: GenreType) => info.id === genre)[0].translation}
+    </S.ItemConentInline>
+  ));
+
+  return (
+    <S.Container>
+      <S.BackDropImg src={backdropImgUrl}></S.BackDropImg>
+      <S.InfoContainer>
+        <S.InfoLeft>
+          <S.Poster src={poster} />
+          <S.ItemTitle>런타임</S.ItemTitle>
+          <S.ItemContent> {runtime}분</S.ItemContent>
+          <S.ItemTitle>장르</S.ItemTitle>
+          <S.GenreContainer>{genreContent}</S.GenreContainer>
+        </S.InfoLeft>
+        <S.InfoRight>
+          <S.FlexContainer>
+            <S.Title>{title}</S.Title>
+            <S.ReleasedYear>({releasedYear})</S.ReleasedYear>
+          </S.FlexContainer>
+          <S.RealTitle>{originTitle}</S.RealTitle>
+          <S.ItemTitle>director</S.ItemTitle>
+          <S.ItemContent>{director.name}</S.ItemContent>
+          <S.ItemTitle>actors</S.ItemTitle>
+          <S.ActorsContainer>{actorsContent}</S.ActorsContainer>
+        </S.InfoRight>
+      </S.InfoContainer>
+      <S.DiscContatiner>
+        <S.ItemTitle>시놉시스</S.ItemTitle>
+        <S.DiscContent>{disc}</S.DiscContent>
+      </S.DiscContatiner>
+    </S.Container>
+  );
 }
 
 export default ContentInfo;
