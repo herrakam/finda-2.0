f4e43a223936fe9c626ad7122588b63364ffa236
herrakam <fm10033@gmail.com>
2023-10-09 18:19:58 +0900

    Refact : Rank 컴포넌트 mockData 변경
    다른 컴포넌트에서 사용할 수 있도록 컴포넌트 외부로 빼고 export 추가

--- finda2.0/src/components/Main/MainBottom/Index.tsx
+++ finda2.0/src/components/Main/MainBottom/Index.tsx
@@ -2,36 +2,38 @@ import * as S from '@components/Main/MainBottom/index.style';
 import Rank from '@components/common/Rank';
 import { RankType } from '@components/common/Rank/type';
 
+export const mockRankInfo = [
+  {
+    title: '테넷',
+    imgSrc:
+      'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+  },
+  {
+    title: '어벤져스',
+    imgSrc:
+      'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+  },
+  {
+    title: '어벤져스',
+    imgSrc:
+      'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+  },
+  {
+    title: '어벤져스',
+    imgSrc:
+      'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+  },
+  {
+    title: '어벤져스',
+    imgSrc:
+      'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
+  },
+];
+
 function MainBottom() {
   const RankProps: RankType = {
     subject: 'top 5',
-    rankInfo: [
-      {
-        title: '테넷',
-        imgSrc:
-          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
-      },
-      {
-        title: '어벤져스',
-        imgSrc:
-          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
-      },
-      {
-        title: '어벤져스',
-        imgSrc:
-          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
-      },
-      {
-        title: '어벤져스',
-        imgSrc:
-          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
-      },
-      {
-        title: '어벤져스',
-        imgSrc:
-          'https://i.namu.wiki/i/_33_6GMFJ2tl_JF2as-w_tL9VnmcrHPMFN5IIz88yFIhLOJVEXfTQNltUZT6CHoc8rGIMOFy7bZmsWPIdq5uFQ.webp',
-      },
-    ],
+    rankInfo: mockRankInfo,
   };
   return (
     <S.MainBottomWrap>
