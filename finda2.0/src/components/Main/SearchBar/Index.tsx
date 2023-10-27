9691179fb5c479047fc7e50abea34dbd5798947a
herrakam <fm10033@gmail.com>
2023-10-27 17:45:32 +0900

    Feat : 검색 기능 구현

--- finda2.0/src/components/Main/SearchBar/Index.tsx
+++ finda2.0/src/components/Main/SearchBar/Index.tsx
@@ -1,11 +1,23 @@
 import * as S from '@components/Main/SearchBar/Index.style';
+import { useState } from 'react';
 import { MdSearch } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 function SearchBar() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const navigate = useNavigate();
+  const getInutValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const gotoSearch = () => {
+    navigate(`/result/${searchValue}`);
+  };
+
   return (
     <S.SearchBarWrap>
-      <S.SearchInput />
-      <S.SearchBtn>
+      <S.SearchInput onChange={getInutValue} />
+      <S.SearchBtn onClick={gotoSearch}>
         <MdSearch size="35" />
       </S.SearchBtn>
     </S.SearchBarWrap>
