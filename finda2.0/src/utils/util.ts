43e4f596ed5fb3ce884a5fefd1b348409098026a
herrakam <fm10033@gmail.com>
2023-10-30 16:35:16 +0900

    Feat : 디바운스 유틸함수 구현

---
+++ finda2.0/src/utils/util.ts
@@ -0,0 +1,12 @@
+interface debouncingType {
+  callback: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  time?: number;
+}
+
+export function debouncing({ callback, time = 500 }: debouncingType) {
+  let timerId: ReturnType<typeof setTimeout> | null = null;
+  return (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (timerId) clearTimeout(timerId);
+    timerId = setTimeout(() => callback(e), time);
+  };
+}
