97b8bf1bc35ed477c6209564ef59ed5e243d56dc
herrakam <fm10033@gmail.com>
2023-11-01 17:33:46 +0900

    Feat : 페이지 이동 함수 구현

--- finda2.0/src/utils/util.ts
+++ finda2.0/src/utils/util.ts
@@ -1,12 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+const navigate = useNavigate();
+
 interface debouncingType {
   callback: (e: React.ChangeEvent<HTMLInputElement>) => void;
   time?: number;
 }
 
-export function debouncing({ callback, time = 500 }: debouncingType) {
+export const debouncing = ({ callback, time = 500 }: debouncingType) => {
   let timerId: ReturnType<typeof setTimeout> | null = null;
   return (e: React.ChangeEvent<HTMLInputElement>) => {
     if (timerId) clearTimeout(timerId);
     timerId = setTimeout(() => callback(e), time);
   };
+};
+
+interface gotoPageProps {
+  url?: 'result' | 'movie';
+  detail: string;
 }
+
+export const gotoPage = ({ url, detail }: gotoPageProps) => {
+  navigate(`/${url}/${detail}`);
+};
