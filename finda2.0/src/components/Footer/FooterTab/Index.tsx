ceb3598ea30bc9a483b5a55da646463a29181478
herrakam <fm10033@gmail.com>
2023-11-04 16:59:04 +0900

    Refact : footer 클릭시 새창으로 열리지 않는 오류 해결
    절대경로 적용, 새창으로 열리는 방식으로 변경

--- finda2.0/src/components/Footer/FooterTab/Index.tsx
+++ finda2.0/src/components/Footer/FooterTab/Index.tsx
@@ -1,8 +1,17 @@
+import { openInNewWindow } from '@/utils/util';
 import * as S from '@components/Footer/FooterTab/Index.style';
 import { FooterTabDataType } from '@components/Footer/Index';
 
 function FooterTab({ url, icon }: FooterTabDataType) {
-  return <S.FooterTab to={url}>{icon}</S.FooterTab>;
+  return (
+    <S.FooterTab
+      onClick={() => {
+        openInNewWindow(url);
+      }}
+    >
+      {icon}
+    </S.FooterTab>
+  );
 }
 
 export default FooterTab;
