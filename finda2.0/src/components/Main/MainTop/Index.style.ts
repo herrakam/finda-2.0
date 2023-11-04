eb7d99995a29c9aa61ed20e60f3f2295a26b8d2e
herrakam <fm10033@gmail.com>
2023-11-04 17:41:04 +0900

    Feat : 서치바 변경 버튼 구현
    클릭시 토글로 서치바를 변경해주는 버튼 구현

--- finda2.0/src/components/Main/MainTop/Index.style.ts
+++ finda2.0/src/components/Main/MainTop/Index.style.ts
@@ -5,7 +5,7 @@ export const MaintopWrap = styled.div`
   ${mixin.flexbox({ dir: 'column', horizontal: 'center', vertical: 'center' })}
   height: 100vh;
   width: 100%;
-  gap: 100px;
+  gap: 50px;
   background: ${({ theme }) => theme.pallete.normalBg};
 `;
 
@@ -14,4 +14,11 @@ export const Copy = styled.div`
   width: 80%;
   ${({ theme }) => theme.typography.Title};
   color: ${({ theme }) => theme.pallete.normalFont};
+`;
+
+export const ChangeSearchBarBtn = styled.button`
+  ${mixin.flexbox({ horizontal: 'center', vertical: 'center' })}
+  ${({ theme }) => theme.typography.Regular}
+  height: 40px;
+  color: ${({ theme }) => theme.pallete.normalBtn};
 `;
--- finda2.0/src/components/Main/MainTop/Index.tsx
+++ finda2.0/src/components/Main/MainTop/Index.tsx
@@ -1,10 +1,22 @@
 import * as S from '@components/Main/MainTop/Index.style';
 import SearchBar from '@components/Main/MainTop/SearchBar/Index';
+import { useState } from 'react';
 
 function MainTop() {
+  const [isGenre, setIsGenre] = useState<boolean>(false);
+
+  const toggleGenre = () => setIsGenre(!isGenre);
+
+  const copy = isGenre
+    ? '장르를 선택해서 영화를 추천받아보세요!'
+    : '영화가 어디서 서비스중인지 찾아보세요!';
+  const buttonText = isGenre ? '영화가 어디있지...?' : '영화 뭐보지...?';
   return (
     <S.MaintopWrap>
-      <S.Copy>영화가 어디서 서비스중인지 알려드려요!</S.Copy>
+      <S.Copy>{copy}</S.Copy>
+      <S.ChangeSearchBarBtn onClick={toggleGenre}>
+        {buttonText}
+      </S.ChangeSearchBarBtn>
       <SearchBar></SearchBar>
     </S.MaintopWrap>
   );
