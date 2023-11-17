18e1c1f68cb6a592715d348b510f186abf0eceb4
herrakam <fm10033@gmail.com>
2023-11-17 18:35:46 +0900

    Refact : poster genreArr 업데이트
    오름차순으로 정렬함

--- finda2.0/src/pages/Scrap.tsx
+++ finda2.0/src/pages/Scrap.tsx
@@ -1,6 +1,6 @@
 import { db } from '@/Firebase';
 import { mixin } from '@/globalStyles/GlobalStyle';
-import { NormalizedDetailType, NormalizedPosterDataType } from '@/utils/type';
+import { NormalizedPosterDataType } from '@/utils/type';
 import { useQuery } from '@tanstack/react-query';
 import {
   DocumentData,
@@ -10,6 +10,7 @@ import {
   orderBy,
   query,
   setDoc,
+  updateDoc,
 } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
@@ -34,24 +35,27 @@ function Scrap() {
     );
   };
 
+  const updatePosterData = async (posterData: posterGenreDataType) => {
+    await updateDoc(doc(db, 'poster', posterData.title), {
+      genreArr: posterData.genreArr,
+    });
+  };
+
   const postDBData = async () => {
     Promise.all(
       posterGenreData.map((result: posterGenreDataType) => {
-        postposterData(result);
+        // postposterData(result);
+        updatePosterData(result);
       }),
     );
   };
 
   const getAllData = async () => {
     const posterRef = collection(db, 'poster');
-    const detailRef = collection(db, 'movies');
     const posterSnap = await getDocs(query(posterRef, orderBy('title')));
-    const detailSnap = await getDocs(query(detailRef, orderBy('title')));
     const posterData: NormalizedPosterDataType[] = [];
-    const detailData: NormalizedDetailType[] = [];
     posterSnap?.forEach((data: DocumentData) => posterData.push(data.data()));
-    detailSnap?.forEach((data: DocumentData) => detailData.push(data.data()));
-    return { posterData, detailData };
+    return { posterData };
   };
 
   const getAllInfo = () =>
@@ -65,13 +69,10 @@ function Scrap() {
   }
   useEffect(() => {
     if (data) {
-      const { posterData: posterInfo, detailData: detailInfo } = data;
+      const { posterData: posterInfo } = data;
       const newPosterData: posterGenreDataType[] = posterInfo
         .map((data: NormalizedPosterDataType): posterGenreDataType => {
-          const genreInfo = detailInfo.find(
-            (detail: NormalizedDetailType) =>
-              data.title === detail.title && detail.genreArr !== undefined,
-          )!.genreArr;
+          const genreInfo = data.genreArr.sort((a, b) => a - b);
           return { title: data.title, genreArr: genreInfo };
         })
         .filter((data: posterGenreDataType) => data.genreArr !== undefined);
