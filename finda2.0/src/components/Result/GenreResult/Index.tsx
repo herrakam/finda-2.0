aec44420bbab2ede11c237a08bb2b4d572bda93f
herrakam <fm10033@gmail.com>
2023-11-30 17:38:40 +0900

    Feat : GenreResult 컴포넌트 구현
    장르 검색시 콘텐츠 가져오고 보여줌

--- finda2.0/src/components/Result/GenreResult/Index.tsx
+++ finda2.0/src/components/Result/GenreResult/Index.tsx
@@ -1,29 +1,63 @@
 import { getSimilarMovies } from '@/utils/API';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '@components/Result/SearchResult/Index.style';
 import { useQuery } from '@tanstack/react-query';
 import { GenreResultPropsType } from './type';
+import { PAGECONTENTCOUNT } from '@/assets/static';
+import Poster from '@components/common/Poster/Index';
+import { NormalizedPosterDataType } from '@/utils/type';
 
 function GenreResult({
-  contentsCount,
-  setContentCount,
   setShowedData,
+  setContentCount,
   showedData,
   genreNumArr,
+  isAbled,
+  setIsAbled,
 }: GenreResultPropsType) {
+  const [curPage, setCurPage] = useState<number>(1);
+
   const getFirstGenreResultInfo = (genreNumArr: number[]) => {
     return useQuery(['getResultQueryKey', genreNumArr], () => {
       return genreNumArr && getSimilarMovies(genreNumArr);
     });
   };
 
+  const getNextDataWithGenre = () => {
+    if (data?.resultData && isAbled) {
+      const cur = curPage * PAGECONTENTCOUNT;
+      const next = (curPage + 1) * PAGECONTENTCOUNT;
+      setShowedData([...showedData, ...data.resultData.slice(cur, next)]);
+      setCurPage(curPage + 1);
+      setIsAbled(false);
+    }
+  };
+
   const { data, isLoading, error } = getFirstGenreResultInfo(genreNumArr);
   if (isLoading) <>로딩중</>;
-  useEffect(() => {}, [data]);
+  if (error) <>에러발생!!!</>;
 
-  console.log(contentsCount, setContentCount, setShowedData, showedData); // 에러 제거용, 변수들 사용시 제거해야함
+  useEffect(() => {
+    if (data) {
+      setShowedData([...data.resultData.slice(0, PAGECONTENTCOUNT)]);
+      setContentCount(data.countContent);
+    }
+  }, [data]);
+  useEffect(() => {
+    if (data) {
+      getNextDataWithGenre();
+    }
+  }, [isAbled]);
 
-  return <S.ResultContatiner></S.ResultContatiner>;
+  const Contents = showedData.map((content: NormalizedPosterDataType) => (
+    <Poster key={content.title} title={content.title} src={content.poster} />
+  ));
+
+  return (
+    <S.ResultContatiner>
+      <S.ConentsContainer>{Contents}</S.ConentsContainer>
+    </S.ResultContatiner>
+  );
 }
 
 export default GenreResult;
