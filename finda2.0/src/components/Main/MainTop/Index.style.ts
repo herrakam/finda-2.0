1c814606cac729edac8bb71870077bb28fd1d6f9
herrakam <fm10033@gmail.com>
2024-02-03 18:46:18 +0900

    Feat : MainTop 반응형 적용

--- finda2.0/src/components/Main/MainTop/Index.style.ts
+++ finda2.0/src/components/Main/MainTop/Index.style.ts
@@ -1,4 +1,4 @@
-import { mixin } from '@/globalStyles/GlobalStyle';
+import { mixin, viewSize } from '@/globalStyles/GlobalStyle';
 import styled from 'styled-components';
 
 export const MaintopWrap = styled.div`
@@ -14,11 +14,18 @@ export const Copy = styled.div`
   width: 80%;
   ${({ theme }) => theme.typography.Title};
   color: ${({ theme }) => theme.pallete.normalFont};
+  @media screen and (${viewSize.mobile}) {
+    ${({ theme }) => theme.typography.SemiTitle};
+    min-width: 300px;
+  }
 `;
 
 export const ChangeSearchBarBtn = styled.button`
   ${mixin.flexbox({ horizontal: 'center', vertical: 'center' })}
   ${({ theme }) => theme.typography.Regular}
   height: 40px;
-  color: ${({ theme }) => theme.pallete.normalBtn};
+  color: ${({ theme }) => theme.pallete.normalFont};
+  background: ${({ theme }) => theme.pallete.normalBtn};
+  border: none;
+  border-radius: 15px;
 `;
