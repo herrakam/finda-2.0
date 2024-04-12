e6461dd94264fc4ec57f6b1adc5719f47834e424
herrakam <fm10033@gmail.com>
2024-04-12 14:25:55 +0900

    Delete : firebase config 주석 제거

--- finda2.0/src/Firebase.ts
+++ finda2.0/src/Firebase.ts
@@ -13,15 +13,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAzkBqlm6vevC7C_FB2R54TRSh-FAWfedg',
-//   authDomain: 'finda-2.firebaseapp.com',
-//   projectId: 'finda-2',
-//   storageBucket: 'finda-2.appspot.com',
-//   messagingSenderId: '694738801055',
-//   appId: '1:694738801055:web:05e1b9c88006a1dbd42daa',
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
