ad5ef579f204ea3a0b3117910db21c1bfcc12b0f
herrakam <fm10033@gmail.com>
2024-05-02 20:49:30 +0900

    Feat : 로그인 컴포넌트 알맞은 위치에 추가
    메인에서 제거, 레이아웃 컴포넌트로 이동

--- finda2.0/src/pages/Layout.tsx
+++ finda2.0/src/pages/Layout.tsx
@@ -1,8 +1,11 @@
+import { isLoginPopUp } from '@/atoms/IsLogin';
 import Error from '@components/Error/Index';
 import Footer from '@components/Footer/Index';
 import Header from '@components/Header/Index';
 import Loading from '@components/Loading/Index';
+import Login from '@components/Login/Index';
 import Panel from '@components/Panel/Index';
+import { useAtomValue } from 'jotai';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
@@ -11,9 +14,12 @@ type IndexType = {
 };
 
 function Layout({ children }: IndexType) {
+  const loginPopUp = useAtomValue(isLoginPopUp);
+
   return (
     <ErrorBoundary FallbackComponent={Error}>
       <Header />
+      {loginPopUp && <Login />}
       <Suspense fallback={<Loading />}>{children}</Suspense>
       <Panel />
       <Footer />
--- finda2.0/src/pages/Main.tsx
+++ finda2.0/src/pages/Main.tsx
@@ -1,11 +1,9 @@
-import Login from '@components/Login/Index';
 import MainTop from '@components/Main/MainTop/Index';
 import PageContainer from '@components/common/PageContainer/Index';
 
 function Main() {
   return (
     <PageContainer>
-      <Login />
       <MainTop />
     </PageContainer>
   );
