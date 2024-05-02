d9a67307ca47b31fc99e7a9d3a2642a262c54a5b
herrakam <fm10033@gmail.com>
2024-05-02 16:53:10 +0900

    Feat : 헤더 로그인 탭 활성화
    isLoginAtom 생성
    로그인 텝에서 참조하던 값 isLoginAtom으로 변경

--- finda2.0/src/components/Header/Index.tsx
+++ finda2.0/src/components/Header/Index.tsx
@@ -1,23 +1,25 @@
+import { isLoginAtom } from '@/atoms/IsLogin';
 import * as S from '@components/Header/Index.style';
 import MiniSearchBar from '@components/Header/miniSearchBar/Index';
+import { useAtomValue } from 'jotai';
 
 function Header() {
-  // const isLogin = true;
-  // const PersonalTab = isLogin ? (
-  //   <>
-  //     <S.HeaderTab to="/myPick">MY Pick</S.HeaderTab>
-  //     <S.HeaderTab to="Logout">LogOut</S.HeaderTab>
-  //   </>
-  // ) : (
-  //   <S.HeaderTab to="/logIn">Login</S.HeaderTab>
-  // );
+  const isLogin = useAtomValue(isLoginAtom);
+  const PersonalTab = isLogin ? (
+    <>
+      <S.HeaderTab to="/myPick">MY Pick</S.HeaderTab>
+      <S.HeaderTab to="Logout">LogOut</S.HeaderTab>
+    </>
+  ) : (
+    <S.HeaderTab to="/logIn">Login</S.HeaderTab>
+  );
 
   return (
     <S.HeaderWrap>
       <S.HeaderLeft>
         <S.Title to="/">FINDA</S.Title>
         <S.HeaderTab to="/result">Movie</S.HeaderTab>
-        {/* {PersonalTab} */}
+        {PersonalTab}
       </S.HeaderLeft>
       <S.HeaderRight>
         <MiniSearchBar />
