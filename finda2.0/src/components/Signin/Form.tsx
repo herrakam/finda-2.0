dbf5e0399b8056e3cbd0644c8b164da8b3073edd
herrakam <fm10033@gmail.com>
2024-05-09 18:57:07 +0900

    Feat : 회원가입 페이지 생성

---
+++ finda2.0/src/components/Signin/Form.tsx
@@ -0,0 +1,9 @@
+function SignInForm() {
+  return (
+    <>
+      <div>회원가입 폼</div>
+    </>
+  );
+}
+
+export default SignInForm;
---
+++ finda2.0/src/pages/SignIn.tsx
@@ -0,0 +1,12 @@
+import SignInForm from '@components/Signin/Form';
+import PageContainer from '@components/common/PageContainer/Index';
+
+function SignIn() {
+  return (
+    <PageContainer size="full">
+      <SignInForm />
+    </PageContainer>
+  );
+}
+
+export default SignIn;
