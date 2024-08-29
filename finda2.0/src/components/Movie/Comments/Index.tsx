b965a7f71084521fb47c51e501f6d656f291f2e8
herrakam <fm10033@gmail.com>
2024-08-29 18:31:10 +0900

    Refact : 댓글 등록 전 validation에 로그인 여부 판단 추가

--- finda2.0/src/components/Movie/Comments/Index.tsx
+++ finda2.0/src/components/Movie/Comments/Index.tsx
@@ -2,6 +2,8 @@ import * as S from '@/components/Movie/Comments/Index.style';
 import { commentDataOutType } from '@/utils/type';
 import { CommentsType } from './type';
 import { extractMonthAndDay } from '@/utils/util';
+import { useAtomValue } from 'jotai';
+import { isLoginAtom } from '@/atoms/IsLogin';
 
 function Comments({
   commentsData,
@@ -12,12 +14,15 @@ function Comments({
   const onPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') validateAndMutate();
   };
-
+  const isLogin = useAtomValue(isLoginAtom);
   const validateAndMutate = () => {
-    if (commentInput !== '') mutate();
-    else window.alert('리뷰를 입력한 뒤 등록해주세요');
+    if (commentInput !== '' && isLogin) mutate();
+    else if (commentInput === '') window.alert('리뷰를 입력한 뒤 등록해주세요');
+    else if (!isLogin)
+      window.alert('로그인한 유저만 댓글을 등록할 수 있습니다. 로그인해주세요');
   };
 
+  console.log(commentsData);
   const commentContent =
     commentsData.length === 0 ? (
       <S.NoComment>첫 리뷰를 남겨보세요!!</S.NoComment>
@@ -26,7 +31,7 @@ function Comments({
         const date = extractMonthAndDay(comment.createdTime.toDate());
         return (
           <S.Comment key={comment.comment}>
-            {/* <S.CommentId>{comment.nickName}</S.CommentId> */}
+            <S.CommentId>{comment.nickName}</S.CommentId>
             <S.CommentContent>{comment.comment}</S.CommentContent>
             <S.CommentCreatedTime>{date}</S.CommentCreatedTime>
           </S.Comment>
